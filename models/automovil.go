c8f1a61743b099d87068daf38e8f33f7164352fc
Reus <victor.villalva2003@hotmail.com>
2023-10-30 20:13:27 -0600

    Car and Parking finish

--- models/automovil.go
+++ models/automovil.go
@@ -1,18 +1,23 @@
 package models
 
 import (
+	"fmt"
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/canvas"
 	"math/rand"
+	"time"
 )
 
 type Automovil struct {
-	Parking       *Parking
-	Identificador int
-	modelo        *canvas.Image
+	Parking         *Parking
+	Identificador   int
+	posicionParking int
+	modelo          *canvas.Image
 }
 
 // m -> modelo
-func CrearAutomivil(p *Parking, m *canvas.Image) *Automovil {
+
+func CrearAutomovil(p *Parking, m *canvas.Image) *Automovil {
 	return &Automovil{
 		Parking: p,
 		modelo:  m,
@@ -22,7 +27,38 @@ func CrearAutomivil(p *Parking, m *canvas.Image) *Automovil {
 func (a *Automovil) MoverCarro() {
 	a.Parking.EspaciosParking <- true
 	a.Parking.M.Lock()
-	x := float32(rand.Intn(650 - 150 + 1))
-	y := float32(rand.Intn(300 - 50 + 1))
 
+	for i := 0; i < len(a.Parking.EspaciosParking); i++ {
+		if !a.Parking.LugaresParking[i].Ocupado {
+			a.modelo.Move(fyne.NewPos(a.Parking.LugaresParking[i].PosicionX, a.Parking.LugaresParking[i].PosicionY))
+			a.modelo.Refresh()
+
+			//Ocupa el lugar
+			a.posicionParking = i
+			a.Parking.LugaresParking[i].Ocupado = true
+			break
+		}
+	}
+
+	fmt.Println("Automovil numero -> #", a.Identificador, " Acaba de entrar")
+	time.Sleep(300 * time.Millisecond)
+	// Desbloqueo de Mutex
+	a.Parking.M.Unlock()
+
+	TiempoEsperaTurno := rand.Intn(5-1+1) + 1
+	time.Sleep(time.Duration(TiempoEsperaTurno) * time.Second)
+
+	// Volvemos a bloquear el Mutex
+	a.Parking.M.Lock()
+
+	//Liberemos espacios de automoviles
+	<-a.Parking.EspaciosParking
+	a.Parking.LugaresParking[a.posicionParking].Ocupado = false
+	a.modelo.Move(fyne.NewPos(0, 80))
+	a.modelo.Refresh()
+	fmt.Println("Automovil numero -> #", a.Identificador, " Acaba de salir")
+	time.Sleep(300 * time.Millisecond)
+
+	//Desbloquemos el Mutex
+	a.Parking.M.Unlock()
 }
--- models/parking.go
+++ models/parking.go
@@ -5,15 +5,51 @@ import (
 	"sync"
 )
 
+// Estructura del lugar
+type Lugar struct {
+	PosicionX float32
+	PosicionY float32
+	Ocupado   bool
+}
+
+// Estructura del estacionamiento
 type Parking struct {
-	EspaciosParking chan bool
-	ColocarCarro    chan *canvas.Image
-	M               sync.Mutex
+	EspaciosParking  chan bool
+	LugaresParking   []Lugar
+	ColocarAutomovil chan *canvas.Image
+	M                sync.Mutex
 }
 
 func CrearParking(nP int) *Parking {
 	return &Parking{
-		EspaciosParking: make(chan bool, nP+1),
-		ColocarCarro:    make(chan *canvas.Image, 1),
+		EspaciosParking:  make(chan bool, nP+1),
+		ColocarAutomovil: make(chan *canvas.Image, 100),
+
+		//Lugares a ocupar dentro del estacionamiento
+		LugaresParking: []Lugar{
+			{PosicionX: 250, PosicionY: 50, Ocupado: false},
+			{PosicionX: 350, PosicionY: 50, Ocupado: false},
+			{PosicionX: 450, PosicionY: 50, Ocupado: false},
+			{PosicionX: 550, PosicionY: 50, Ocupado: false},
+			{PosicionX: 650, PosicionY: 50, Ocupado: false},
+
+			{PosicionX: 250, PosicionY: 150, Ocupado: false},
+			{PosicionX: 350, PosicionY: 150, Ocupado: false},
+			{PosicionX: 450, PosicionY: 150, Ocupado: false},
+			{PosicionX: 550, PosicionY: 150, Ocupado: false},
+			{PosicionX: 650, PosicionY: 150, Ocupado: false},
+
+			{PosicionX: 250, PosicionY: 300, Ocupado: false},
+			{PosicionX: 350, PosicionY: 300, Ocupado: false},
+			{PosicionX: 450, PosicionY: 300, Ocupado: false},
+			{PosicionX: 550, PosicionY: 300, Ocupado: false},
+			{PosicionX: 650, PosicionY: 300, Ocupado: false},
+
+			{PosicionX: 250, PosicionY: 400, Ocupado: false},
+			{PosicionX: 350, PosicionY: 400, Ocupado: false},
+			{PosicionX: 450, PosicionY: 400, Ocupado: false},
+			{PosicionX: 550, PosicionY: 400, Ocupado: false},
+			{PosicionX: 650, PosicionY: 400, Ocupado: false},
+		},
 	}
 }
